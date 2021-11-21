50110be3e837cee8c4dc638555ea38395eaee12b
Dereje Desta <ddesta777@gmail.com>
2021-11-21 11:31:14 +0300

    added 7-add_tuple.py, 12-switch.py and minor fixes for 3 and 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     list_cp = my_list[:]
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return list_cp
     list_cp[idx] = element
     return list_cp
