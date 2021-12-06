7d3040b8f08dbdb792d2b0384a89e1b7cdadf3b6
Dereje Desta <ddesta777@gmail.com>
2021-12-06 15:16:42 +0300

    added 0-safe_print_list.py 1-safe_print_integer.py 2-safe_print_list_integers.py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+def safe_print_list_integers(my_list=[], x=0):
+    j = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+        except (ValueError, TypeError):
+            continue
+        j += 1
+    print()
+    return j
