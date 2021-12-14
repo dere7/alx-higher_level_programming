4361d7c10c71dfd13081723ec1150c8f99760230
Dereje Desta <ddesta777@gmail.com>
2021-12-14 15:29:02 +0300

    Minor fixes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,17 +1,40 @@
 #!/usr/bin/python3
+'''Defines Rectangle having width and height
+'''
 
 
 class Rectangle:
+    '''Defines Rectange class having print_symbol and number_of_instance'''
+
     def __init__(self, width=0, height=0):
+        '''initializes Rectangle object
+        Args:
+            width(int): width of rectangle
+            height(int): height of rectangle
+            '''
         self.width = width
         self.height = height
 
+    def __str__(self):
+        '''str represenation of Rectangle Object'''
+        string = ''
+        for i in range(self.height):
+            for j in range(self.width):
+                string += str(self.print_symbol)
+            string += '\n' if self.width != 0 and i + 1 != self.height else ''
+        return string
+
     @property
     def width(self):
+        '''gets width'''
         return self.__width
 
     @width.setter
     def width(self, width):
+        '''sets width of the rectangle
+        Args:
+            width(int): width of the rectangle
+        '''
         if type(width) is not int:
             raise TypeError('width must be an integer')
         if width < 0:
@@ -20,10 +43,15 @@ class Rectangle:
 
     @property
     def height(self):
+        '''gets the height of the rectangle object'''
         return self.__height
 
     @height.setter
     def height(self, height):
+        '''sets height of the rectangle
+        Args:
+            height(int): height of the rectangle
+        '''
         if type(height) is not int:
             raise TypeError('height must be an integer')
         if height < 0:
--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,17 +1,32 @@
 #!/usr/bin/python3
+'''Defines Rectangle having width and height
+'''
 
 
 class Rectangle:
+    '''Defines Rectange class having print_symbol and number_of_instance'''
+    print_symbol = '#'
+
     def __init__(self, width=0, height=0):
+        '''initializes Rectangle object
+        Args:
+            width(int): width of rectangle
+            height(int): height of rectangle
+            '''
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        '''gets width'''
         return self.__width
 
     @width.setter
     def width(self, width):
+        '''sets width of the rectangle
+        Args:
+            width(int): width of the rectangle
+        '''
         if type(width) is not int:
             raise TypeError('width must be an integer')
         if width < 0:
@@ -20,10 +35,15 @@ class Rectangle:
 
     @property
     def height(self):
+        '''gets the height of the rectangle object'''
         return self.__height
 
     @height.setter
     def height(self, height):
+        '''sets height of the rectangle
+        Args:
+            height(int): height of the rectangle
+        '''
         if type(height) is not int:
             raise TypeError('height must be an integer')
         if height < 0:
@@ -31,7 +51,9 @@ class Rectangle:
         self.__height = height
 
     def area(self):
+        '''returns the area of the rectangle'''
         return self.height * self.width
 
     def perimeter(self):
+        '''returns the perimeter of the rectangle'''
         return 2 * (self.height + self.width)
