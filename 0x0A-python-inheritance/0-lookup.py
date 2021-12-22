9fd14f21806f7c56e2fe1051d420f0871b5b601c
Dereje Desta <ddesta777@gmail.com>
2021-12-22 18:06:46 +0300

    Initial commit for inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+'''returns the list of available attributes and methods of an obj'''
+def lookup(obj):
+    '''lookup - returns a list of attri and methods obj have'''
+    return dir(obj)
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''MyList is subclass of list'''
+
+
+class MyList(list):
+    '''MyList inherits list class'''
+    def print_sorted(self):
+        '''prints sorted list'''
+        print(sorted(self))
