c54c7fd1eea079ecb4e48c711a7578022d2b1a66
Dereje Desta <ddesta777@gmail.com>
2021-12-27 11:50:37 +0300

    added 7-add_item.py 11-student.py 12-pascal_triangle.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,33 +1,19 @@
 #!/usr/bin/python3
+"""
+adds all arguments to a list and saves them to a file
+"""
+from sys import argv
+save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
 
-"""7-add_item
-Write a script that adds all arguments to a Python list, and then save them
-to a file"""
+filename = "add_item.json"
 
+try:
+    json_list = load_from_json_file(filename)
+except FileNotFoundError:
+    json_list = []
 
-def main():
-    """Program starts here"""
-    filename = "add_item.json"
-    argv = __import__("sys").argv
-    load_from_json_file = __import__(
-        '6-load_from_json_file').load_from_json_file
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+for arg in argv[1:]:
+    json_list.append(arg)
 
-    try:
-
-        py_objs = load_from_json_file(filename)
-
-        for i in range(len(argv)):
-            if i == 0:
-                continue
-            py_objs.append(argv[i])
-
-        save_to_json_file(py_objs, filename)
-    except FileNotFoundError:
-        with open(filename, "w") as f:
-            f.write("[]")
-            return
-
-
-if __name__ == "__main__":
-    main()
+save_to_json_file(json_list, filename)
