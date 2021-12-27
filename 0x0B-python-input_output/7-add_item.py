f5e25260c0c29449d37cbbe856b7f1212b6c50f6
Dereje Desta <ddesta777@gmail.com>
2021-12-27 12:13:44 +0300

    added 100 and 101 for I/0

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,18 +2,16 @@
 """
 adds all arguments to a list and saves them to a file
 """
-from sys import argv
-save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
 
-filename = "add_item.json"
-
-try:
-    json_list = load_from_json_file(filename)
-except FileNotFoundError:
-    json_list = []
-
-for arg in argv[1:]:
-    json_list.append(arg)
-
-save_to_json_file(json_list, filename)
+if __name__ == '__main__':
+    argv = __import__("sys").argv
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    filename = 'add_item.json'
+    try:
+        args = load_from_json_file(filename)
+    except FileNotFoundError:
+        args = []
+    for i in range(1, len(argv)):
+        args.append(argv[i])
+    save_to_json_file(args, filename)
