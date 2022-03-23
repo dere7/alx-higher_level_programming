c6fb26bebc137937464f4efbed58efaa6d433440
Dereje Desta <ddesta777@gmail.com>
2022-03-23 18:11:58 +0300

    worked on 6-peak of 0x10-python-network_0 project

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,8 +2,19 @@
 """contains a function that finds a peak in list"""
 
 
-def find_peak(ls):
+def find_peak(lst):
     """finds a peak in list"""
-    for i in range(1, len(ls) - 1):
-        if ls[i - 1] < ls[i] > ls[i + 1]:
-            return ls[i]
+    start, end = 0, len(lst) - 1
+    while start < end:
+        mid = start + (end - start) // 2
+
+        # Check if it's a peak
+        if lst[mid] > lst[mid - 1] and lst[mid] > lst[mid + 1]:
+            return lst[mid]
+        if len(lst) == 3 and lst[mid] == lst[mid-1] == lst[mid+1]:
+            return lst[mid]
+        if lst[mid - 1] > lst[mid + 1]:
+            end = mid
+        else:
+            start = mid + 1
+    return None
